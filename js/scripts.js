f1670e08e52e92233c48fd331c9b9c95fed31ae4
monicanq <monicanq@gmail.com>
2019-09-09 19:26:09 +0200

    Added JavaScript functionality to the cards so that they flip, and if there is a match they stay unflipped.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,55 @@
-< script >
-  document.getElementsByClassName('flip-card').addEventListener("click", flipping);
+//Variable declaration
+var cards = document.querySelectorAll('.flip-card');
+let firstClick = false;
+let lockGame = false;
+let firstCard, secondCard;
 
-function flipping() {
-  document.getElementById("demo").innerHTML = Date();
-} <
-/script>
+//function assignments
+function flipCard () {
+  if (lockGame) return;
+  if (this === firstCard) return;
+  this.classList.add('flip');
+  if (!firstClick){
+    firstClick = true;
+    firstCard = this;
+  } else {
+    firstClick = false;
+    secondCard = this;
+    matchCheck();
+  }
+}
+function matchCheck (){
+  if (firstCard.dataset.card === secondCard.dataset.card) {
+    blockCards();
+    resetGame ();
+  } else {
+    resetCards();
+  }
+}
+function blockCards (){
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+}
+function resetCards(){
+  lockGame = true;
+  setTimeout (()=>{
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+    resetGame();
+  }, 1500);
+}
+function resetGame(){
+  [firstClick, lockGame] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function Shuffle(){
+  cards.forEach(card => {
+    let randomOrder = Math.floor(Math.random()*12);
+    card.style.order = randomOrder;
+  });
+})();
+
+for (var i = 0; i < cards.length; i++) {
+  cards[i].addEventListener("click",flipCard);
+}
